c27b998aec87590c08ea243f2ec6dbce0b85e8ae
John Doe <shamailov@gmail.com>
2021-05-01 19:15:23 +0300

    Prevent insert empty tasks

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Alert } from 'react-bootstrap';
 import TodoModel from '../../model/todoModel';
 import './NewTask.css'
 
 export default function NewTask({handleNewTask}) {
 
-
+    const [wrongInput, setWrongInput] = useState(false)
     const handleKeyDown = (event) => {
+        setWrongInput(false)
         if (event.key === 'Enter') {
-            handleNewTask(new TodoModel(event.target.value))
-            event.target.value=""
+            if (event.target.value.length>1)
+            {
+                handleNewTask(new TodoModel(event.target.value))
+                event.target.value=""
+            }
+            else
+                setWrongInput(true)
         }
     }
 
     return (
         <div className="new-task">
             <input type="text"className="new-task-input" placeholder="What's next?" onKeyDown={handleKeyDown} />
+            {
+                wrongInput?
+                <Alert variant="danger">Your task is too short. try at least 2 characters</Alert>:null
+            }
         </div>
     )
 }
